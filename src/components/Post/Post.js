7b031297f7df69e2866fa68ef387f00a4f12a8c0
Nurullo Sulaymonov <sulaimonovnn@gmail.com>
2020-11-06 18:06:17 +0500

    Events &amp; State

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,26 +1,58 @@
 import React from 'react'
-import Tags from '../Tags/Tags';
 import './Post.css'
+import Tags from '../Tags/Tags';
 
-function Post({ post }) {
+function Post({ post, onLike, onRemove, onHide, onShow }) {
     const { author } = post;
     const { photo } = post;
 
+    const handleRemove = (evn) => {
+        onRemove();
+    }
+    const handleHide = (evn) => {
+        onHide();
+    }
+    const handleShow = (evn) => {
+        onShow();
+    }
+
+    const handleClick = (evt) => {
+        onLike();
+    }
+
+    if (post.hidden) {
+        return (
+            <article>
+                <header>
+                    <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name} />
+                    <h5>{author.name}</h5>
+                    <button onClick={handleShow}>показать</button>
+                </header>
+            </article>
+        )
+    }
     return (
         <article>
             <header>
-                <img className="Post-avatar" src={author.avatar} width="50" height="50" alt={author.name} />
-                <h5>{post.author.name}</h5>
+                <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name} />
+                <h5>{author.name}</h5>
+                <button onClick={handleRemove}>удалить</button>
+                <button onClick={handleHide}>скрыть</button>
                 <div>{post.created}</div>
-                {post.hit ? <span>HIT</span> : null}
+                {post.hit && <span>HIT</span>}
             </header>
             <div>
-                {<div className="Post-content">{post.content}</div>}
+                <div className="Post-content">{post.content}</div>
                 {photo && <img src={photo.url} alt={photo.alt} className="Post-photo" />}
             </div>
             <footer>
-                <span className="Post-likes">
-                    <img src={"https://lms.openjs.io/" + (post.likedByMe === true ? "" : "un") + "liked.svg"} width="20" height="20" alt="likes" />
+                <span className="Post-likes" onClick={handleClick}>
+                    <img
+                        src={post.likedByMe ? "https://lms.openjs.io/liked.svg" : "https://lms.openjs.io/unliked.svg"}
+                        alt="likes"
+                        width="20"
+                        height="20"
+                    />
                     <span className="Post-likes-count">{post.likes}</span>
                     {post.tags && <Tags tags={post.tags} />}
                 </span>
--- src/components/Wall/Wall.js
+++ src/components/Wall/Wall.js
@@ -1,28 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 import Post from '../Post/Post'
 
 function Wall() {
-    const posts = [
+
+    const [posts, setPosts] = useState([
         {
             id: 2,
             author: {
                 id: 1,
                 avatar: 'https://lms.openjs.io/logo_js.svg',
-                name: 'OpenJs'
+                name: 'OpenJS'
             },
             content: 'Ну как вы справились с домашкой?',
             photo: null,
             hit: true,
             likes: 222,
             likedByMe: true,
-            created: 1603501200
+            hidden: false,
+            tags: ['deadline', 'homework'],
+            created: 1603774800
         },
         {
             id: 1,
             author: {
                 id: 1,
                 avatar: 'https://lms.openjs.io/logo_js.svg',
-                name: 'OpenJs'
+                name: 'OpenJS'
             },
             content: null,
             photo: {
@@ -31,15 +35,59 @@ function Wall() {
             },
             hit: true,
             likes: 10,
-            likedByMe: true,
-            tags: ['deadline', 'homework'],
+            likedByMe: false,
+            hidden: true,
             created: 1603501200
         }
-    ]
+    ])
+
+    const handlePostLike = (id) => {
+        setPosts((prevNewPosts) => {
+            return prevNewPosts.map(post => {
+                if (post.id !== id) {
+                    return post
+                }
+                return { ...post, likedByMe: !post.likedByMe, likes: post.likedByMe ? post.likes - 1 : post.likes + 1 }
+            })
+        });
+    };
+
+    const handlePostRemove = (id) => {
+
+        setPosts((prevState) => prevState.filter(o => o.id !== id));
+    }
+
+    const handlePostHide = (id) => {
+        setPosts((prevNewPosts) => {
+            return prevNewPosts.map(post => {
+                if (post.id !== id) {
+                    return post
+                }
+                return { ...post, hidden: !post.hidden }
+            })
+        });
+    }
+
+    const handlePostShow = (id) => {
+        setPosts((prevState) => prevState.map(o => {
+            if (o.id !== id) {
+                return o;
+            }
+
+            const hidden = false;
+            return { ...o, hidden };
+        }));
+    }
 
     return (
         <div>
-            {posts.map(p => <Post key={p.id} post={p} />)}
+            {posts.map(p =>
+                <Post key={p.id}
+                    post={p}
+                    onLike={() => handlePostLike(p.id)}
+                    onRemove={() => handlePostRemove(p.id)}
+                    onHide={() => handlePostHide(p.id)}
+                    onShow={() => handlePostShow(p.id)} />)}
         </div>
     );
 }
